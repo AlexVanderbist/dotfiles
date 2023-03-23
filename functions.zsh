d4d70c02d28882cd27648251841c91ae31d913eb
Alex Vanderbist <alex.vanderbist@gmail.com>
2023-03-23 14:32:29 +0100

    Update functions.zsh

--- functions.zsh
+++ functions.zsh
@@ -85,3 +85,16 @@ function silent() {
    "$@" >& /dev/null
 }
 
+function toggle-dock() {
+    if [[ "$(defaults read com.apple.dock autohide)" -eq "1" ]]; then
+        defaults write com.apple.dock autohide -bool false && killall Dock
+	defaults delete com.apple.dock autohide-delay && killall Dock
+	defaults write com.apple.dock no-bouncing -bool FALSE && killall Dock
+	exit
+    fi
+
+    defaults write com.apple.dock autohide -bool true && killall Dock
+    defaults write com.apple.dock autohide-delay -float 1000 && killall Dock
+    defaults write com.apple.dock no-bouncing -bool TRUE && killall Dock
+}
+
